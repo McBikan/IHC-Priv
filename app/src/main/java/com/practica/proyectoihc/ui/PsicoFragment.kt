a6762e9549904bead25efb2ee6663fb50ff51956
ravigod22 <ravi22135@gmail.com>
2025-06-22 14:34:06 -0500

    Update

--- app/src/main/java/com/practica/proyectoihc/ui/PsicoFragment.kt
+++ app/src/main/java/com/practica/proyectoihc/ui/PsicoFragment.kt
@@ -13,21 +13,12 @@ import com.practica.proyectoihc.model.Pregunta
 import com.practica.proyectoihc.model.Opcion
 import java.io.InputStreamReader
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [PsicoFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
 class PsicoFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     // UI
     private lateinit var tvPregunta: TextView
     private lateinit var radioGroup: RadioGroup
@@ -40,14 +31,6 @@ class PsicoFragment : Fragment() {
     private val puntajesPorBloque = mutableMapOf<Int, Int>()
     private var indiceActual = 0
     private var puntajeTotal = 0
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -159,24 +142,4 @@ class PsicoFragment : Fragment() {
         }
     }
 
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment PsicoFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            PsicoFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
 }
