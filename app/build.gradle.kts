bf61280b8442530ad26fa9c4a2ebd41b10bf9a5d
McBikan <brianjetfenix@gmail.com>
2025-06-25 01:16:53 -0500

    brian cambios

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.safeargs)
+    alias(libs.plugins.google.services)
 }
 
 android {
@@ -66,4 +67,7 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.auth)
 }
